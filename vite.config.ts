93dd0da647bfafb0064cd1ea484ac127ab2b2c13
lalala3006 <330688363@qq.com>
2024-11-17 19:11:30 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  base: 'https://github.com/lalala3006/lalalaTest.git' // 替换为你的 GitHub 仓库名
+})
+
